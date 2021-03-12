280a806f25fd81d7ebc2336b731b44b13869ba01
syChoi2 <minamcra@hotmail.com>
2021-03-12 15:07:18 +0900

    회원가입 로직 수정에 따른 api 추가 , 수정

--- src/main/java/com/coronacapsule/api/controller/UserController.java
+++ src/main/java/com/coronacapsule/api/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.coronacapsule.api.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.coronacapsule.api.dto.LoginRequestDto;
 import com.coronacapsule.api.dto.LoginResponseDto;
-import com.coronacapsule.api.dto.NicknameDto;
+import com.coronacapsule.api.dto.SignUpDto;
 
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -19,14 +21,22 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/v1/users")
 public class UserController {
 
-
+	/**
+	 * 회원 유무 확인
+	 */
+	@ApiOperation(value="로그인")
+	@GetMapping("/exists")
+	public ResponseEntity<Boolean> exists(String socialToken){
+		
+		return ResponseEntity.ok(null);
+		
+	}
+	
 	/**
 	 * 로그인
-	 * 최초 가입일 경우 닉네임을 받기 위한 flag 값 필요
 	 *
 	 * return
 	 * 1. 토큰 + 캡슐정보
-	 * 2. new user t/f
 	 */
 	@ApiOperation(value="로그인")
 	@PostMapping("/login")
@@ -37,13 +47,26 @@ public class UserController {
 	}
 	
 	/**
-	 * 최초 로그인 시 닉네임 등록/수정
+	 * 회원가입
+	 * 
+	 * 회원가입 후 로그인까지 진행
+	 */
+	@ApiOperation(value="회원가입")
+	@PatchMapping("/signUp")
+	public ResponseEntity<LoginResponseDto> signUp(@RequestBody SignUpDto signUpDto){
+		
+		return ResponseEntity.ok(null);
+		
+	}	
+	
+	/**
+	 * 닉네임 수정
 	 * 중복일 시 오류 메시지 출력
 	 */
-	@ApiOperation(value="닉네임 등록/수정")
+	@ApiOperation(value="닉네임 수정")
 	@ApiImplicitParam(name = "Autentication", paramType = "header", required = true, value = "access token")
-	@PostMapping("/nickname")
-	public ResponseEntity<LoginResponseDto> setNickname(@RequestBody NicknameDto nicknameDto){
+	@PatchMapping("/nickname")
+	public ResponseEntity<?> setNickname(@RequestBody SignUpDto nicknameDto){
 		
 		return ResponseEntity.ok(null);
 		
